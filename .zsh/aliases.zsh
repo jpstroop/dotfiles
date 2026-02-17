0ef5ad7b578670e6484fded53c9e38bc9efdf40a
Jon Stroop <jsgh@jopast.io>
2026-02-16 23:59:03 -0500

    Exclude dotfiles from zsh command correction

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,6 @@
 
 # Fix ls colors for GNU coreutils (OMZ sets -G which is BSD-only)
 alias ls='ls -a --color=auto'
+
+# Prevent zsh correction from suggesting .zsh directory
+alias zsh='nocorrect zsh'
