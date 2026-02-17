7148a926620a4b14bc6c3d856ded3487f00b7ab0
Jon Stroop <jsgh@jopast.io>
2026-02-16 23:00:02 -0500

    Add -a flag to ls alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,4 @@
 # Custom aliases (loaded after OMZ, so these override plugin aliases)
 
 # Fix ls colors for GNU coreutils (OMZ sets -G which is BSD-only)
-alias ls='ls --color=auto'
+alias ls='ls -a --color=auto'
