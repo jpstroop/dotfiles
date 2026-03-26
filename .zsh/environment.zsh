484724880c9c51d239388f9291bb78228c7b5981
Jon Stroop <jsgh@jopast.io>
2026-03-26 11:04:27 -0400

    google cloud sdk

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -8,3 +8,9 @@ export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/b
 
 # Claude Code
 export PATH="$HOME/.local/bin:$PATH"
+
+# Google Cloud SDK
+if [ -f '$HOME/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/google-cloud-sdk/path.zsh.inc'; fi
+
+# Shell command completion for gcloud
+if [ -f '$HOME/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/google-cloud-sdk/completion.zsh.inc'; fi
